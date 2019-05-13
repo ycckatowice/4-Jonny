2cf7e88dc4c673876667928f4bbb291dd229688f
Mikołaj Woźniak <inkubator@mozesz.pl>
2019-05-13 18:29:22 +0200

    he who doesn't try, doesn't achieve

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>moja strona</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1> Strona Janka</h1>
+	<div id="container">
+		<div class="box"> czerwony1</div>
+		<div class="box"> czerwony2</div>
+		<div class="box"> czerwony3</div>
+		<div class="box yellowbox"> żółty1</div>
+		<div class="box greenbox"> zielony</div>
+		<div class="box yellowbox"> żółty2</div>
+	</div>
+	<div class="container"></div>
+</body>
+</html>
